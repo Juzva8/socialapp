91052f72b9e4fbd01e3a1162102f31a06197f324
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-26 10:50:28 -0800

    adding firebase

---
+++ src/firebas.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQpOueEdlVLvMS70DbGK9sPUkCMrdZ4Ik",
+    authDomain: "socialapp-a6247.firebaseapp.com",
+    projectId: "socialapp-a6247",
+    storageBucket: "socialapp-a6247.appspot.com",
+    messagingSenderId: "196090038910",
+    appId: "1:196090038910:web:29079203ac7a9b3c72262f",
+    measurementId: "G-GS0NRVPD69"
+  };
+  
