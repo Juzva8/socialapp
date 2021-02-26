a30c422619e8907b68920e141c7ef1b14d8ce55a
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-26 15:06:18 -0800

    done with side row for now

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,7 +13,7 @@ import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow src="https://github.com/settings/profile" title="Lawrence Juzva" />
+            <SidebarRow src="https://avatars.githubusercontent.com/u/72834046?s=400&u=13200316b9a5b284830062c1c1bf2a347616e043&v=4" title="Lawrence Juzva" />
             <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center"/>
             <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
             <SidebarRow Icon={PeopleIcon} title='Friends' />     
