8b25be5b340dfbc08b4bfd7110bd25af43854254
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-26 16:22:42 -0800

    styling sender

--- src/components/MessageSender/MessageSender.js
+++ src/components/MessageSender/MessageSender.js
@@ -1,6 +1,9 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './MessageSender.css'
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 function MessageSender() {
     const handleSubmit = (e) => {
@@ -20,7 +23,18 @@ function MessageSender() {
                 </from>
              </div>
             <div className ="messageSender__bottom">
-
+        <div className ="messageSender__option">
+             <VideocamIcon style={{color: "red"}} />
+             <h3>Live Video</h3>
+                </div>
+                <div className ="messageSender__option">
+             <PhotoLibraryIcon style={{color: "green"}} />
+             <h3>Photo/Video</h3>
+                </div>
+                <div className ="messageSender__option">
+             <InsertEmoticonIcon style={{color: "orange"}} />
+             <h3>Feeling/Activity</h3>
+                </div>
             </div>
         </div>
     )
