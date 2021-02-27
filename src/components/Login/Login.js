7635c732e971bc53320c684768a960f31046af25
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-27 13:57:25 -0800

    added google auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,15 @@ import './Login.css'
 import logo from '../Assets/logo.png'
 import log from '../Assets/login_logo2.png'
 import { Button } from '@material-ui/core'
+import { auth, provider } from '../../firebase'
 
 function Login() {
     const signIn = () => {
-
+auth.signInWithPopup(provider)
+.then(result => {
+    console.log(result.user) 
+})
+.catch((error) => alert(error.message))
     };
     return (
         <div className="login">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCQpOueEdlVLvMS70DbGK9sPUkCMrdZ4Ik",
     authDomain: "socialapp-a6247.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     measurementId: "G-GS0NRVPD69"
   };
   
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
