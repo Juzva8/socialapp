9c708db1f0982b962676f5c8159930c261ba7eed
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-27 14:45:29 -0800

    works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar'
-import Feed from './components/Feed/Feed'
+import Sidebar from './components/Sidebar/Sidebar';
+import Feed from './components/Feed/Feed';
 import Widgets from './components/Widgets/Widgets';
-import Login from './components/Login/Login'
+import Login from './components/Login/Login';
+import { useStateValue } from './StateProvider'
 
 
 function App() {
-  const user = null;
+  const [{ user }, dispatch] = useStateValue();
   return (
     <div className="app">
       {!user ? (
