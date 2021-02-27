73e10d0ecfd0e156a0c3147da9298d49ddd5f9f9
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-27 14:41:46 -0800

    working on parsing in user data

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,11 +4,19 @@ import logo from '../Assets/logo.png'
 import log from '../Assets/login_logo2.png'
 import { Button } from '@material-ui/core'
 import { auth, provider } from '../../firebase'
+import { actionTypes } from '../../reducer'
+import { useStateValue } from '../../StateProvider'
 
 function Login() {
+    const [state, dispatch] = useStateValue(); 
+
     const signIn = () => {
 auth.signInWithPopup(provider)
 .then(result => {
+    dispatch({
+    type: actionTypes.SET_USER,
+    user: result.user,
+    })
     console.log(result.user) 
 })
 .catch((error) => alert(error.message))
