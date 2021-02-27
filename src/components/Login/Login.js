f9cb892b49c724f8dd1fe6ad9dab65cba580ae2e
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-27 13:20:51 -0800

    styling login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Login.css'
+import logo from '../Assets/logo.png'
+import log from '../Assets/login_logo2.png'
+import { Button } from '@material-ui/core'
 
 function Login() {
+    const signIn = () => {
+
+    };
     return (
         <div className="login">
-            
+            <div className="login__logo">
+                <img src={logo} alt="logo" />
+
+                <img src={log} alt="login logo" />
+            </div>
+            <Button type="submit" onClick={signIn}>
+                Sign In
+            </Button>
         </div>
     )
 }
