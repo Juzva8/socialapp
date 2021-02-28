c2fb7af2faa0ee84487c0392d79791f4eebfb9a8
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-27 17:13:27 -0800

    deploying

--- src/components/MessageSender/MessageSender.js
+++ src/components/MessageSender/MessageSender.js
@@ -1,17 +1,28 @@
-import { Avatar } from '@material-ui/core'
-import React, { useState } from 'react'
-import './MessageSender.css'
+import { Avatar } from '@material-ui/core';
+import React, { useState } from 'react';
+import './MessageSender.css';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
-import { useStateValue } from "../../StateProvider"
+import { useStateValue } from "../../StateProvider";
+import db from '../../firebase';
+import firebase from 'firebase';
 
 function MessageSender() {
     const [{ user }, dispatch] = useStateValue();
     const[input, setInput] = useState('');
     const[imageUrl, setImageUrl] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        db.collection('posts').add({
+            message:input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            profilePic: user.photoURL,
+            username: user.displayName,
+            image: imageUrl
+        })
 
         setInput("");
         setImageUrl("");
